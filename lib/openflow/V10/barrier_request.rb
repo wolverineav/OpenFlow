71ed95c9e153447f9d506783fa3a8e4adc901264
Sovietaced <Sovietaced@gmail.com>
2014-02-22 04:57:11 -0800

    grr

--- lib/openflow/V10/barrier_request.rb
+++ lib/openflow/V10/barrier_request.rb
@@ -1,7 +1,7 @@
 module OpenFlow
-	module V10
-		class BarrierRequest < Message
-			initial_value.type = 10 
-		end
-	end
-end	
+  module V10
+    class BarrierRequest < Message
+      initial_value.type = 10 
+    end
+  end
+end 
