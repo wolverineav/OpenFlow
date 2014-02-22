5ce53a14d86576cfd403180f200b5ba0ed8217d7
Sovietaced <Sovietaced@gmail.com>
2014-02-22 04:54:50 -0800

    fix tabs

--- lib/openflow/V10/echo_reply.rb
+++ lib/openflow/V10/echo_reply.rb
@@ -1,12 +1,10 @@
 module OpenFlow
 	module V10
 		class EchoReply < Message
-			
 			# Override Message type
 			initial_value.type = 3
-			
+			# New fields
 			rest :data, "Data"
-
 		end
 	end
 end	
--- lib/openflow/V10/echo_request.rb
+++ lib/openflow/V10/echo_request.rb
@@ -1,12 +1,10 @@
 module OpenFlow
 	module V10
-		class EchoRequest < Message
-			
+		class EchoRequest < Message		
 			# Override Message type
 			initial_value.type = 2
-
+			# New fields
 			rest :data, "Data"
-
 		end
 	end
 end	
