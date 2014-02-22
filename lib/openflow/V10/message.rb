6c02d739fb160755f1b07cde347aea6860d41b16
Sovietaced <Sovietaced@gmail.com>
2014-02-22 05:03:30 -0800

    tabs to spaces git attribute

--- lib/openflow/V10/message.rb
+++ lib/openflow/V10/message.rb
@@ -1,17 +1,17 @@
 require 'bit-struct'
-   
+  
 module OpenFlow
-	module V10
-		class Message < BitStruct	
-			unsigned :version, 8, "OF Version"
-			unsigned :type, 8, "Type"
-			unsigned :len, 16, "Message Length"
-			unsigned :xid, 32, "Transfer ID"
+  module V10
+    class Message < BitStruct	
+      unsigned :version, 8, "OF Version"
+      unsigned :type, 8, "Type"
+      unsigned :len, 16, "Message Length"
+      unsigned :xid, 32, "Transfer ID"
 
-			initial_value.version = 1
-			initial_value.type = 0
-			initial_value.len = 0
-			initial_value.xid = 0
-		end
-	end
+      initial_value.version = 1
+      initial_value.type = 0
+      initial_value.len = 0
+      initial_value.xid = 0
+    end
+  end
 end
