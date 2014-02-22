834efc8abc0abb76f7801deb4070481e8adfe3fd
Sovietaced <Sovietaced@gmail.com>
2014-02-22 04:50:26 -0800

    echos

---
+++ lib/openflow/V10/echo_reply.rb
@@ -0,0 +1,12 @@
+module OpenFlow
+	module V10
+		class EchoReply < Message
+			
+			# Override Message type
+			initial_value.type = 3
+			
+			rest :data, "Data"
+
+		end
+	end
+end	
---
+++ lib/openflow/V10/echo_request.rb
@@ -0,0 +1,12 @@
+module OpenFlow
+	module V10
+		class EchoRequest < Message
+			
+			# Override Message type
+			initial_value.type = 2
+
+			rest :data, "Data"
+
+		end
+	end
+end	
